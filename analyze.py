886c9e7097a20102fc3c2ff8708daadec2eec5d4
xgao@zoyoe.com <xgao@zoyoe.com>
2021-02-12 20:44:22 +0800

    add test pairs

--- analyze.py
+++ analyze.py
@@ -5,8 +5,7 @@ from rejson import Client, Path
 rj = Client(host='localhost', port=6379, decode_responses=True)
 
 print("Current timestamp", get_current_timestamp())
-all_pairs = rj.jsonget("pairs").values()
-print(all_pairs)
-#all_pairs = json.load(open('files/main_pairs.json'))
+#all_pairs = rj.jsonget("pairs").values()
+all_pairs = json.load(open('uni-pairs.json'))
 analysis = firmware.Analysis(all_pairs)
 analysis.scan_cycles()
